74ee9c4f7e278a4554b2096fce22ea74cbac0795
Yulia Erusha <erushnikova.talisman@gmail.com>
2022-10-04 22:38:24 +0500

    задача 8

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), на выходе показывает все четные числа от 1 до N.
+
+using System;
+class homework
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+    }
+}
+
